555cdd4f63343c8cb5200f0aa9dcc36f140ac3e7
sanya <billion1cash1337@gmail.com>
2025-12-03 08:46:25 +0700

    refactoring DataBaseService.AuthorizeUser() method

--- TaskManager/Infrastucture/Network/DataBaseService.cs
+++ TaskManager/Infrastucture/Network/DataBaseService.cs
@@ -14,31 +14,15 @@ namespace TaskManager.Infrastucture.Network
     {
         private static readonly string _host = "5.129.192.166";
         private static readonly Uri _uri = new Uri($"http://{_host}/rmpPhpApi/api/");
-
-        public static HttpClient client = new HttpClient();
+        private static HttpClient client = new HttpClient();
         
         public static async Task<UserResponse> AuthorizeUser(UserRequest userRequestObj)
         {
-            UserResponse userResponseObj = new UserResponse();
-
             if (!String.IsNullOrEmpty(userRequestObj.username))
             {
-                // dev only 
-                userRequestObj = new UserRequest
-                {
-                    username = "sanyament",
-                    password = "chachatop"
-                };
-                
-                
-                // serialize request object
-                StringContent content = new StringContent(JsonSerializer.Serialize(userRequestObj), Encoding.UTF8, "application/json");
-
-                // dev only
-                string reqCon = await content.ReadAsStringAsync();
-                MessageBox.Show("StringContent (request) = " + reqCon, "info");
-                
-                // flush request
+                StringContent content = new StringContent(
+                    JsonSerializer.Serialize(userRequestObj), 
+                    Encoding.UTF8, "application/json");
                 HttpResponseMessage httpResponseMessage = await client.PostAsync(_uri + "login/login.php", content);
 
                 // deserialize response
@@ -46,32 +30,32 @@ namespace TaskManager.Infrastucture.Network
                 {
                     JsonNode responseRootJson;
                     // getting root JsonNode from httpResponseMessage
-                    try
+                    try { responseRootJson = JsonNode.Parse(await httpResponseMessage.Content.ReadAsStringAsync()); }
+                    catch (Exception ex) { throw new Exception("DataBaseService error \"getting clear JsonNode from httpResponseMessage\""); }
+                    
+                    // checking and return result
+                    if (responseRootJson["success"].GetValue<bool>())
                     {
-                        responseRootJson = JsonNode.Parse(await httpResponseMessage.Content.ReadAsStringAsync());
+                        JsonObject responseUserJson = responseRootJson["user"].AsObject();
+                        try
+                        {
+                            return new UserResponse
+                            {
+                                username = responseUserJson["username"].GetValue<string>(),
+                                idRole = (int)responseUserJson["idRole"].GetValue<int>()
+                            };
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception($"DataBaseService error: \"getting UserResponse from responseUserJson\"\nMessage = {ex.Message}");
+                        }
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        throw new Exception("DataBaseService error \"getting clear JsonNode from httpResponseMessage\"");
+                        throw new Exception("Неверный логин или пароль!");
                     }
-                    // getting JsonNode subarray user from responseRootJson
-                    JsonObject responseUserJson = responseRootJson["user"].AsObject();
-                    // getting UserResponse from responseUserJson
-                    try
-                    {
-                    userResponseObj.username = responseUserJson["username"].GetValue<string>();
-                    userResponseObj.idRole = (int)responseUserJson["idRole"].GetValue<int>();
-                    }
-                    catch (Exception ex)
-                    {
-                        throw new Exception($"DataBaseService error: \"getting UserResponse from responseUserJson\"\nMessage = {ex.Message}");
-                    }
-                    return userResponseObj;
                 }
-                else
-                {
-                    throw new Exception("error on DataBaseService \"AuthorizeUser()\"\nhttp code = " + httpResponseMessage.StatusCode);
-                }
+                else throw new Exception("error on DataBaseService \"AuthorizeUser()\"\nhttp code = " + httpResponseMessage.StatusCode);
             }
             throw new Exception("userObj.username is null or empty");
         }
